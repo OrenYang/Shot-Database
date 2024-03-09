4a75f1d480f0c7277771ce6ddc3ff0be89676b9f
OrenYang <orenyang@gmail.com>
2024-03-09 01:07:14 -0800

    Updated shot detail view and added diagnostic models

--- shot-website/ceszar/shots/admin.py
+++ shot-website/ceszar/shots/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Gas, GasConfig, Shot, DiagnosticImage
+from .models import Gas, GasConfig, Shot, XrayDetector, Filter, XuvImage
 
 # Register your models here.
 admin.site.register(Gas)
 admin.site.register(GasConfig)
 admin.site.register(Shot)
-admin.site.register(DiagnosticImage)
+admin.site.register(XrayDetector)
+admin.site.register(Filter)
+admin.site.register(XuvImage)
--- shot-website/ceszar/shots/models.py
+++ shot-website/ceszar/shots/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 def image_upload_dir(instance, filename):
-    return 'images/{}/{}'.format(instance.diagnostic, filename)
+    return 'images/{}/{}'.format(instance.name, filename)
 
 def config(self):
     config=''
@@ -50,9 +50,8 @@ class Shot(models.Model):
 
     time = models.DateTimeField()
     num = models.IntegerField(null=True, blank=True)
-    gasConfig = models.ForeignKey(GasConfig, on_delete=models.PROTECT, null=True, related_name='gasConfig')
-    current = models.FloatField(null=True, blank=True)
 
+    gasConfig = models.ForeignKey(GasConfig, on_delete=models.PROTECT, null=True, related_name='shot')
     outer_press = models.FloatField(null=True, blank=True)
     inner_press = models.FloatField(null=True, blank=True)
     target_press = models.FloatField(null=True, blank=True)
@@ -60,6 +59,18 @@ class Shot(models.Model):
     inner_timing = models.IntegerField(null=True, blank=True)
     target_timing = models.IntegerField(null=True, blank=True)
 
+    current = models.FloatField(null=True, blank=True)
+    current_time = models.FloatField(null=True, blank=True)
+    dip_time = models.FloatField(null=True, blank=True)
+
+    cavity_v = models.FloatField(null=True, blank=True, default=60)
+    cavity_p = models.FloatField(null=True, blank=True, default=77)
+    trigger_v = models.FloatField(null=True, blank=True, default=60)
+    trigger_p = models.FloatField(null=True, blank=True, default=35)
+    maxwell_p = models.FloatField(null=True, blank=True, default=60)
+    premag_v = models.FloatField(null=True, blank=True)
+    premag_t = models.FloatField(null=True, blank=True)
+    pressure = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         con = str(self.gasConfig)
@@ -69,23 +80,41 @@ class Shot(models.Model):
         return reverse('shot-detail', args=[str(self.num)])
 
 
-class DiagnosticImage(models.Model):
+class Filter(models.Model):
 
-    choices = [
-        ('xuv','xuv'),
-        ('schlieren','schlieren'),
-        ('other','other'),
-    ]
-
-    diagnostic = models.CharField(max_length=20,
-                                  choices=choices,
-                                  default='other',)
-    shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name='shot', null=True)
-    comments = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to=image_upload_dir)
+    mat = models.CharField(max_length=50)
+    thickness = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return '{} image from shot {}'.format(self.diagnostic, self.shot.num)
+        return self.mat
 
-    def get_absolute_url(self):
-        return reverse('shot-detail', kwargs={'pk': self.shot.pk})
+class XrayDetector(models.Model):
+
+    shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name='xrayDetector', null=True)
+    num = models.IntegerField(null=True, blank=True)
+    filter = models.ForeignKey(Filter, on_delete=models.CASCADE, related_name='xrayDetector', null=True)
+
+    start = models.FloatField(null=True, blank=True)
+    peak_time = models.FloatField(null=True, blank=True)
+    peak_volt = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return 'XD{} from shot {}'.format(self.num, self.shot.num)
+
+
+class XuvImage(models.Model):
+
+    name = 'xuv'
+    shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name='xuvImage', null=True)
+    num = models.IntegerField(null=True, blank=True)
+    charge = models.FloatField(null=True, blank=True)
+
+    frame1 = models.FloatField(null=True, blank=True)
+    frame2 = models.FloatField(null=True, blank=True)
+    frame3 = models.FloatField(null=True, blank=True)
+    frame4 = models.FloatField(null=True, blank=True)
+
+    image = models.ImageField(upload_to=image_upload_dir, null=True, blank=True)
+
+    def __str__(self):
+        return 'XUV Camera {} from shot {}'.format(self.num, self.shot.num)

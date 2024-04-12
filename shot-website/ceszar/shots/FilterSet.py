dfea68baf1cb1c220ba072ba67838e6d70bc6968
OrenYang <orenyang@gmail.com>
2024-04-12 14:09:59 -0700

    Added more filters

--- shot-website/ceszar/shots/FilterSet.py
+++ shot-website/ceszar/shots/FilterSet.py
@@ -6,12 +6,20 @@ class ShotFilter(django_filters.FilterSet):
     current__gt = django_filters.NumberFilter(field_name='current', lookup_expr='gt')
     current__lt = django_filters.NumberFilter(field_name='current', lookup_expr='lt')
 
+    current_time__gt = django_filters.NumberFilter(field_name='current_time', lookup_expr='gt')
+    current_time__lt = django_filters.NumberFilter(field_name='current_time', lookup_expr='lt')
+
     gasConfig__target__gas = django_filters.CharFilter(lookup_expr='icontains')
     gasConfig__inner__gas = django_filters.CharFilter(lookup_expr='icontains')
     gasConfig__outer__gas = django_filters.CharFilter(lookup_expr='icontains')
 
-    time = django_filters.TimeFilter(field_name='time',lookup_expr='icontains')
-    date = django_filters.DateFilter(field_name='date',lookup_expr='icontains')
+    time__gt = django_filters.TimeFilter(field_name='time',lookup_expr='gt')
+    time__lt = django_filters.TimeFilter(field_name='time',lookup_expr='lt')
+    date__gt = django_filters.DateFilter(field_name='date',lookup_expr='gt')
+    date__lt = django_filters.DateFilter(field_name='date',lookup_expr='lt')
+
+    xrayDetector__peak_time__gt = django_filters.NumberFilter(field_name='xrayDetector__peak_time',lookup_expr='gt')
+    xrayDetector__peak_time__lt = django_filters.NumberFilter(field_name='xrayDetector__peak_time', lookup_expr='lt')
 
     o = django_filters.OrderingFilter(
             fields=(
@@ -23,4 +31,4 @@ class ShotFilter(django_filters.FilterSet):
 
     class Meta:
         model = Shot
-        fields = ['current','gasConfig','time','date']
+        fields = ['current','gasConfig','time','date','current_time']
